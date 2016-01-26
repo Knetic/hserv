26f232af804fc6596fc38e98a06b020fd71403d3
= <glester491@gmail.com>
2016-01-25 23:23:12 -0800

    Default port to 80, using 8080 as non-sudo backup

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import (
     "net/http"
+	"fmt"
+	"strings"
 )
 
 func main() {
+
+	var err error
+
     http.Handle("/", http.FileServer(http.Dir("./")))
-    http.ListenAndServe(":8080", nil)
+
+	err = http.ListenAndServe(":80", nil)
+	if(err != nil && strings.Contains(err.Error(), "permission")) {
+
+		fmt.Printf("Bind permission when trying to serve on :80 (are you sudo?). Serving on :8080 instead.\n")
+
+		err = http.ListenAndServe(":8080", nil)
+		fmt.Printf("%v\n", err)
+	}
 }
