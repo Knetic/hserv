19ffcf0a0eb7a17f184a1231793a743e7106b1b8
= <glester491@gmail.com>
2016-01-25 22:52:09 -0800

    Works. Hopefully final commit?

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "fmt"
+    "net/http"
 )
 
 func main() {
-
-  fmt.Printf("Hello, hserv\n")
+    http.Handle("/", http.FileServer(http.Dir("./")))
+    http.ListenAndServe(":8080", nil)
 }
