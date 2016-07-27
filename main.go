be16acf78945896d9e29ac0612a2912ccb060c1d
George Lester <glester491@gmail.com>
2016-07-27 11:00:12 -0700

    fmt

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "net/http"
 	"fmt"
+	"net/http"
 	"strings"
 )
 
@@ -10,10 +10,10 @@ func main() {
 
 	var err error
 
-    http.Handle("/", http.FileServer(http.Dir("./")))
+	http.Handle("/", http.FileServer(http.Dir("./")))
 
 	err = http.ListenAndServe(":80", nil)
-	if(err != nil && strings.Contains(err.Error(), "permission")) {
+	if err != nil && strings.Contains(err.Error(), "permission") {
 
 		fmt.Printf("Bind permission when trying to serve on :80 (are you sudo?). Serving on :8080 instead.\n")
 
